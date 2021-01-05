d03bb1a347d4d7f3a9072dc35d53b9a1886df557
GregSCooper2514 <kirill.shestov123@gmail.com>
2021-01-05 18:02:16 +0300

    lolz

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg 
+from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg
 import os
+import sys
 import time
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtWidgets import QLabel, QLineEdit
 os.chdir("C:\\Users\\Greg\\Downloads")
 start_time = None
 end_time = None
+KEYWIDTH, KEYHEIGHT = 18, 72
 
 
 class PianoKey(QtWidgets.QGraphicsRectItem):
@@ -13,7 +15,7 @@ class PianoKey(QtWidgets.QGraphicsRectItem):
         super(PianoKey, self).__init__(rect, parent)
         self.m_pressed = False
         self.m_selectedBrush = QtGui.QBrush()
-        self.m_brush = QtGui.QBrush(QtCore.Qt.black) if black else QtGui.QBrush(QtCore.Qt.white) 
+        self.m_brush = QtGui.QBrush(QtCore.Qt.black) if black else QtGui.QBrush(QtCore.Qt.white)
         self.m_black = black
         self.m_number = 0
 
@@ -23,15 +25,15 @@ class PianoKey(QtWidgets.QGraphicsRectItem):
     def paint(self, painter, option, widget):
         rendered = QtSvg.QSvgRenderer("key.svg")
         black_pen = QtGui.QPen(QtCore.Qt.black, 1)
-        gray_pen = QtGui.QPen(QtGui.QBrush(QtCore.Qt.gray), 1, 
+        gray_pen = QtGui.QPen(QtGui.QBrush(QtCore.Qt.gray), 1,
             QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin)
         if self.m_pressed:
             if self.m_selectedBrush.style() != QtCore.Qt.NoBrush:
                 painter.setBrush(self.m_selectedBrush)
             else:
                 painter.setBrush(QtWidgets.QApplication.palette().highlight())
-        else: 
-            painter.setBrush(self.m_brush);
+        else:
+            painter.setBrush(self.m_brush)
         painter.setPen(black_pen)
         painter.drawRoundedRect(self.rect(), 15, 15, QtCore.Qt.RelativeSize)
         if self.m_black:
@@ -50,7 +52,6 @@ class PianoKey(QtWidgets.QGraphicsRectItem):
         self.m_pressed = True
         self.update()
         start_time = time.time()
-        #lol2(str(self.m_brush))
         super(PianoKey, self).mousePressEvent(event)
         event.accept()
 
@@ -62,42 +63,44 @@ class PianoKey(QtWidgets.QGraphicsRectItem):
         addnote(self.m_number, start_time, end_time)
         super(PianoKey, self).mouseReleaseEvent(event)
 
-KEYWIDTH, KEYHEIGHT = 18, 72
 
 class PianoKeyBoard(QtWidgets.QGraphicsView):
-    def __init__(self, num_octaves=2,  parent=None, end_octave=1, frst_octave=0):
+    def __init__(self, num_octaves=2,  parent=None, end_octave=1, first_octave=3):
         super(PianoKeyBoard, self).__init__(parent)
         self.initialize()
         self.m_numOctaves = num_octaves
         scene = QtWidgets.QGraphicsScene(QtCore.QRectF(0, 0, KEYWIDTH * self.m_numOctaves * 7, KEYHEIGHT), self)
         self.setScene(scene)
-        numkeys = self.m_numOctaves * 12 + end_octave + frst_octave
+        numkeys = self.m_numOctaves * 12 + end_octave + first_octave
         shift_octave = 0
-        if frst_octave > 0:
-            if frst_octave % 12 < 9:
-                shift_octave = (frst_octave % 12) // 2 + 1
+        if first_octave > 0:
+            if first_octave % 12 < 9:
+                shift_octave = (first_octave % 12) // 2 + 1
             else:
-                shift_octave = (frst_octave % 12 + 1) // 2 + 1
+                shift_octave = (first_octave % 12 + 1) // 2 + 1
 
         for i in range(numkeys):
-            octave = (i - shift_octave)//12*7
+            octave = 0
             if shift_octave > 0 and i > 0:
-                if i > frst_octave % 12:
-                    j = (i - frst_octave % 12) % 12
+                if i >= first_octave % 12:
+                    octave = (i - first_octave % 12) // 12 * 7 + shift_octave
+                    j = (i - first_octave % 12) % 12
                     if j >= 5: j += 1
                 else:
-                    pass                
-
-                   
+                    if (first_octave - i) < 4: #?????????
+                        j = i
+                    else:
+                        j = i + 1
             else:
+                octave = i // 12 * 7
                 j = i % 12
                 if j >= 5: j += 1
             if j % 2 == 0:
-                x = (octave + j/2 + shift_octave)*KEYWIDTH
+                x = (octave + j / 2)*KEYWIDTH
                 key = PianoKey(rect=QtCore.QRectF(x, 0, KEYWIDTH, KEYHEIGHT), black=False, num=i)
             else:
-                x = (octave + j//2 + shift_octave) * KEYWIDTH  + KEYWIDTH * 6//10 + 1
-                key = PianoKey(rect=QtCore.QRectF(x, 0, KEYWIDTH * 8//10 - 1, KEYHEIGHT * 6//10 ), black=True)
+                x = (octave + j // 2) * KEYWIDTH  + KEYWIDTH * 6 // 10 + 1
+                key = PianoKey(rect=QtCore.QRectF(x, 0, KEYWIDTH * 8 // 10 - 1, KEYHEIGHT * 6//10), black=True)
                 key.setZValue(1)
             key.setPressedBrush(QtWidgets.QApplication.palette().highlight())
             key.m_number = i
@@ -110,28 +113,31 @@ class PianoKeyBoard(QtWidgets.QGraphicsView):
         self.setCacheMode(QtWidgets.QGraphicsView.CacheBackground)
         self.setViewportUpdateMode(QtWidgets.QGraphicsView.MinimalViewportUpdate)
         self.setRenderHints(QtGui.QPainter.Antialiasing|
-            QtGui.QPainter.TextAntialiasing | 
+            QtGui.QPainter.TextAntialiasing |
             QtGui.QPainter.SmoothPixmapTransform)
         self.setOptimizationFlag(QtWidgets.QGraphicsView.DontClipPainter, True)
         self.setOptimizationFlag(QtWidgets.QGraphicsView.DontSavePainterState, True)
         self.setOptimizationFlag(QtWidgets.QGraphicsView.DontAdjustForAntialiasing, True)
         self.setBackgroundBrush(QtWidgets.QApplication.palette().base())
 
-    def resizeEvent(self, event):
-        super(PianoKeyBoard, self).resizeEvent(event)
-        self.fitInView(self.scene().sceneRect(), QtCore.Qt.KeepAspectRatio)
+    #def resizeEvent(self, event):
+    #    super(PianoKeyBoard, self).resizeEvent(event)
+    #    self.fitInView(self.scene().sceneRect(), QtCore.Qt.KeepAspectRatio)
 
     def sizeHint(self):
         return self.mapFromScene(self.sceneRect()).boundingRect().size()
+
 
 def addnote(note, start_time, end_time):
     x = str(end_time - start_time) + " " + str(note)
     lol1.setText(x)
 
+
 def display_num(text):
     lol1.setText(text)
+
+
 if __name__ == '__main__':
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     app.setStyle('fusion')
     lol = QPushButton("Вывести длительность")
