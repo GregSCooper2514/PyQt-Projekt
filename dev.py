3b60313c9242fde942ffee0b109e512a8455c8f2
GregSCooper2514 <kirill.shestov123@gmail.com>
2021-01-09 03:33:18 +0300

    -

--- dev.py
+++ dev.py
@@ -1,15 +1,11 @@
 from mingus.containers import Bar
-from mingus.containers import Track
 import mingus.extra.lilypond as LilyPond
 import os
 os.chdir("C:\\Users\\Greg\\Downloads")
 b = Bar()
-b1 = Bar()
-b.place_notes("Bb-4", 4)
-b.place_notes("E-4", 16)
-b1.place_notes("C-2", 1)
-t = Track()
-t.add_bar(b)
-t.add_bar(b1)
-bar = LilyPond.from_Track(t)
-LilyPond.to_png(bar, "my_first_barr")
+b + "C-0"
+b + "C#-0"
+b + "D-0"
+b + "E-20"
+bar = LilyPond.from_Bar(b)
+LilyPond.to_png(bar, "my_first_bar")
--- main.py
+++ main.py
@@ -3,13 +3,32 @@ import sys
 import time
 from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtGui import *
+from mingus.containers import Bar
+import mingus.extra.LilyPond as LilyPond
 os.chdir("C:\\Users\\Greg\\Downloads")
+note_list = dict()
+note_list = {"0": "C-0", "1": "C#-0", "2": "D-0", "3": "D#-0", "4": "E-0", "5": "E#-0", "6": "F-0",
+             "7": "F#-0", "8": "G-0", "9": "G#-0", "10": "A-0", "11": "A#-0", "12": "B-0",
+             "13": "B#-0", "14": "C-1", "15": "C#-1", "16": "D-1", "17": "D#-1", "18": "E-1",
+             "19": "E#-1", "20": "F-1", "21": "F#-1", "22": "G-1", "23": "G#-1", "24": "A-1", 
+             "25": "A#-1", "26": "B-1", "27": "B#-1", "27": "C-2", "28": "C#-2", "29": "D-2",
+             "29": "D#-2", "30": "E-2", "31": "E#-2", "32": "F-2", "33": "F#-2", "34": "G-2",
+             "35": "G#-2", "36": "A-2", "37": "A#-2", "38": "B-2", "39": "B#-2", "40": "C-3", 
+             "41": "C#-3", "42": "D-3", "43": "D#-3", "44": "E-3", "45": "E#-3", "46": "F-3", 
+             "47": "F#-3", "48": "G-3", "49": "G#-3", "50": "A-3", "51": "A#-3", "52": "B-3", 
+             "53": "B#-3", "54": "C-4", "55": "C#-4", "56": "D-4", "57": "D#-4", "58": "E-4", 
+             "59": "E#-4", "60": "F-4", "61": "F#-4", "62": "G-4", "63": "G#-4", "64": "A-4", 
+             "65": "A#-4", "66": "B-4", "67": "B#-4", "68": "C-5", "69": "C#-5", "70": "D-5", 
+             "71": "D#-5", "72": "E-5", "73": "E#-5", "74": "F-5", "75": "F#-5", "76": "G-5", 
+             "77": "G#-5", "78": "A-5", "79": "A#-5", "80": "B-5", "81": "B#-5", "82": "C-6", 
+             "83": "C#-6", "84": "D-6", "85": "D#-6", "86": "E-6", "87": "E#-6"}
 start_time = None
 end_time = None
 menubar = None
 buttton_lay = None
 KEYWIDTH, KEYHEIGHT = 18, 72
+note_container = []
 
 
 class Example(QtWidgets.QWidget):
@@ -95,7 +114,7 @@ class PianoKey(QtWidgets.QGraphicsRectItem):
         self.m_pressed = False
         self.update()
         end_time = time.time()
-        addnote(self.m_number, start_time, end_time)
+        add_note(self.m_number, start_time, end_time)
         super(PianoKey, self).mouseReleaseEvent(event)
 
 
@@ -196,17 +215,28 @@ class NoteTable(QtWidgets.QGraphicsView):
 
 
 def add_note(note, start_time, end_time):
-    x = str(end_time - start_time) + " " + str(note)
-    lol1.setText(x)
+    global note_container
+    lol1.setText(str(note))
+    element = [note, end_time - start_time]
+    note_container.append(element)
+
+
+def get_png():
+    global note_container
+    b = Bar()
+    for a in note_container:
+        pass    
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     app.setStyle('fusion')
     ex = Example()
+    lol1 = QLineEdit()
     lay = QtWidgets.QVBoxLayout(ex)
     lay.addWidget(menubar)
     lay.addLayout(buttton_lay)
+    lay.addWidget(lol1)
     lay.addWidget(NoteTable())
     lay.addWidget(PianoKeyBoard())
     ex.show()
