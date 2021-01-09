4182e67bd35494404df7bdad4a3f3bbe30e1ed77
GregSCooper2514 <kirill.shestov123@gmail.com>
2021-01-09 05:02:23 +0300

    -

--- dev.py
+++ dev.py
@@ -1,11 +1,42 @@
 from mingus.containers import Bar
+from mingus.containers import Track
 import mingus.extra.lilypond as LilyPond
+from mingus.containers.instrument import Instrument, Piano, Guitar
+i = Instrument()
+i.name = "Lol"
+i.clef = "bass"
 import os
 os.chdir("C:\\Users\\Greg\\Downloads")
-b = Bar()
-b + "C-0"
-b + "C#-0"
-b + "D-0"
-b + "E-20"
-bar = LilyPond.from_Bar(b)
+b1 = Bar()
+b1 + "C-4"
+b2 = Bar()
+b2 + "E-4"
+b3 = Bar()
+b3 + "C-4"
+b4 = Bar()
+b4 + "E-4"
+b5 = Bar()
+b5 + "C-4"
+b6 = Bar()
+b6 + "E-4"
+b7 = Bar()
+b7 + "C-4"
+b8 = Bar()
+b8 + "E-4"
+b9 = Bar()
+b9 + "C-4"
+b10 = Bar()
+b10 + "E-4"
+t = Track(instrument="Lol")
+t.add_bar(b1)
+t.add_bar(b2)
+t.add_bar(b3)
+t.add_bar(b4)
+t.add_bar(b5)
+t.add_bar(b6)
+t.add_bar(b7)
+t.add_bar(b8)
+t.add_bar(b9)
+t.add_bar(b10)
+bar = LilyPond.from_Track(t)
 LilyPond.to_png(bar, "my_first_bar")
--- main.py
+++ main.py
@@ -5,17 +5,22 @@ from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from mingus.containers import Bar
-import mingus.extra.LilyPond as LilyPond
+from mingus.containers import Track
+import mingus.extra.lilypond as LilyPond
+from mingus.containers.instrument import Instrument, Piano, Guitar
+i = Instrument()
+i.name = "PianoKeyBoard"
+i.clef = "bass"
 os.chdir("C:\\Users\\Greg\\Downloads")
 note_list = dict()
 note_list = {"0": "C-0", "1": "C#-0", "2": "D-0", "3": "D#-0", "4": "E-0", "5": "E#-0", "6": "F-0",
              "7": "F#-0", "8": "G-0", "9": "G#-0", "10": "A-0", "11": "A#-0", "12": "B-0",
              "13": "B#-0", "14": "C-1", "15": "C#-1", "16": "D-1", "17": "D#-1", "18": "E-1",
-             "19": "E#-1", "20": "F-1", "21": "F#-1", "22": "G-1", "23": "G#-1", "24": "A-1", 
+             "19": "E#-1", "20": "F-1", "21": "F#-1", "22": "G-1", "23": "G#-1", "24": "A-1",
              "25": "A#-1", "26": "B-1", "27": "B#-1", "27": "C-2", "28": "C#-2", "29": "D-2",
              "29": "D#-2", "30": "E-2", "31": "E#-2", "32": "F-2", "33": "F#-2", "34": "G-2",
-             "35": "G#-2", "36": "A-2", "37": "A#-2", "38": "B-2", "39": "B#-2", "40": "C-3", 
-             "41": "C#-3", "42": "D-3", "43": "D#-3", "44": "E-3", "45": "E#-3", "46": "F-3", 
+             "35": "G#-2", "36": "A-2", "37": "A#-2", "38": "B-2", "39": "B#-2", "40": "C-3",
+             "41": "C#-3", "42": "D-3", "43": "D#-3", "44": "E-3", "45": "E#-3", "46": "F-3",
              "47": "F#-3", "48": "G-3", "49": "G#-3", "50": "A-3", "51": "A#-3", "52": "B-3", 
              "53": "B#-3", "54": "C-4", "55": "C#-4", "56": "D-4", "57": "D#-4", "58": "E-4", 
              "59": "E#-4", "60": "F-4", "61": "F#-4", "62": "G-4", "63": "G#-4", "64": "A-4", 
@@ -219,13 +224,20 @@ def add_note(note, start_time, end_time):
     lol1.setText(str(note))
     element = [note, end_time - start_time]
     note_container.append(element)
+    get_png()
 
 
 def get_png():
     global note_container
-    b = Bar()
+    t = Track(instrument=i)
+    lol = Bar()
+    t.add_bar(lol)
     for a in note_container:
-        pass    
+        b = Bar("C", (1, 1))
+        b + note_list[str(a[0])]
+        t.add_bar(t)
+    bar = LilyPond.from_Track(t)
+    LilyPond.to_png(bar, "lol")
 
 
 if __name__ == '__main__':
