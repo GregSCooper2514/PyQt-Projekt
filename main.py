75129416ed807b314b97dfb54a54d05c5f871e6a
GregSCooper2514 <kirill.shestov123@gmail.com>
2021-01-11 05:27:25 +0300

    -

--- main.py
+++ main.py
@@ -4,9 +4,25 @@ import time
 import csv
 from pydub import *
 from pydub.playback import play
-from PyQt5 import QtCore, QtGui, QtWidgets, QtSvg
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5.QtSvg import *
+note_list = {"0": "C-0", "1": "C#-0", "2": "D-0", "3": "C-1", "4": "C#-1", "5": "D-1",
+             "6": "D#-1", "7": "E-1", "8": "E#-1", "9": "F-1", "10": "F#-1", "11": "G-1",
+             "12": "G#-1", "13": "A-1", "14": "A#-1", "15": "B-1", "16": "B#-1", "17": "C-2",
+             "18": "C#-2", "19": "D-2", "20": "D#-2", "21": "E-2", "22": "E#-2", "23": "F-2",
+             "24": "F#-2", "25": "G-2", "26": "G#-2", "27": "A-2", "28": "A#-2", "29": "B-2",
+             "30": "B#-2", "31": "C-3", "32": "C#-3", "33": "D-3", "34": "D#-3", "35": "E-3",
+             "36": "E#-3", "37": "F-3", "38": "F#-3", "39": "G-3", "40": "G#-3", "41": "A-3",
+             "42": "A#-3", "43": "B-3", "44": "B#-3", "45": "C-4", "46": "C#-4", "47": "D-4",
+             "48": "D#-4", "49": "E-4", "50": "E#-4", "51": "F-4", "52": "F#-4", "53": "G-4",
+             "54": "G#-4", "55": "A-4", "56": "A#-4", "57": "B-4", "58": "B#-4", "59": "C-5",
+             "60": "C#-5", "61": "D-5", "62": "D#-5", "63": "E-5", "64": "E#-5", "65": "F-5",
+             "66": "F#-5", "67": "G-5", "68": "G#-5", "69": "A-5", "70": "A#-5", "71": "B-5",
+             "72": "B#-5", "73": "C-6", "74": "C#-6", "75": "D-6", "76": "D#-6", "77": "E-6",
+             "78": "E#-6", "79": "F-6", "80": "F#-6", "81": "G-6", "82": "G#-6", "83": "A-6",
+             "84": "A#-6", "85": "B-6", "86": "B#-6", "87": "C-7"}
 os.chdir("C:\\Users\\Greg\\Downloads")
 default_sound = AudioSegment.from_wav("default.wav")
 start_time = None
@@ -17,7 +33,7 @@ KEYWIDTH, KEYHEIGHT = 18, 72
 note_container = []
 
 
-class Example(QtWidgets.QWidget):
+class Example(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -42,7 +58,7 @@ class Example(QtWidgets.QWidget):
         self.action_sas = QAction("Save as sound file", self)
         self.actionFile.addAction(self.action_sas)
         self.action_sas.triggered.connect(self.save_as_sound)
-        self.lay_button = QtWidgets.QHBoxLayout()
+        self.lay_button = QHBoxLayout()
         self.play_but = QPushButton()
         self.play_but.setText("PLAY")
         self.play_but.clicked.connect(play_sound)
@@ -97,12 +113,12 @@ class Example(QtWidgets.QWidget):
                 note_container.append(i[1])
             pass
 
-class PianoKey(QtWidgets.QGraphicsRectItem):
-    def __init__(self, black=False, rect = QtCore.QRectF(), parent=None, num=0):
+class PianoKey(QGraphicsRectItem):
+    def __init__(self, black=False, rect = QRectF(), parent=None, num=0):
         super(PianoKey, self).__init__(rect, parent)
         self.m_pressed = False
-        self.m_selectedBrush = QtGui.QBrush()
-        self.m_brush = QtGui.QBrush(QtCore.Qt.black) if black else QtGui.QBrush(QtCore.Qt.white)
+        self.m_selectedBrush = QBrush()
+        self.m_brush = QBrush(Qt.black) if black else QBrush(Qt.white)
         self.m_black = black
         self.m_number = 0
 
@@ -110,28 +126,28 @@ class PianoKey(QtWidgets.QGraphicsRectItem):
         self.m_selectedBrush = brush
 
     def paint(self, painter, option, widget):
-        rendered = QtSvg.QSvgRenderer("key.svg")
-        black_pen = QtGui.QPen(QtCore.Qt.black, 1)
-        gray_pen = QtGui.QPen(QtGui.QBrush(QtCore.Qt.gray), 1, QtCore.Qt.SolidLine, QtCore.Qt.RoundCap, QtCore.Qt.RoundJoin)
+        rendered = QSvgRenderer("key.svg")
+        black_pen = QPen(Qt.black, 1)
+        gray_pen = QPen(QBrush(Qt.gray), 1, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin)
         if self.m_pressed:
-            if self.m_selectedBrush.style() != QtCore.Qt.NoBrush:
+            if self.m_selectedBrush.style() != Qt.NoBrush:
                 painter.setBrush(self.m_selectedBrush)
             else:
-                painter.setBrush(QtWidgets.QApplication.palette().highlight())
+                painter.setBrush(QApplication.palette().highlight())
         else:
             painter.setBrush(self.m_brush)
         painter.setPen(black_pen)
-        painter.drawRoundedRect(self.rect(), 15, 15, QtCore.Qt.RelativeSize)
+        painter.drawRoundedRect(self.rect(), 15, 15, Qt.RelativeSize)
         if self.m_black:
             rendered.render(painter, self.rect())
         else:
             points = [
-                QtCore.QPointF(self.rect().left()+1.5, self.rect().bottom()-1),
-                QtCore.QPointF(self.rect().right()-1, self.rect().bottom()-1),
-                QtCore.QPointF(self.rect().right()-1, self.rect().top()+1)
+                QPointF(self.rect().left()+1.5, self.rect().bottom()-1),
+                QPointF(self.rect().right()-1, self.rect().bottom()-1),
+                QPointF(self.rect().right()-1, self.rect().top()+1)
             ]
             painter.setPen(gray_pen)
-            painter.drawPolyline(QtGui.QPolygonF(points))
+            painter.drawPolyline(QPolygonF(points))
 
     def mousePressEvent(self, event):
         global start_time
@@ -150,12 +166,12 @@ class PianoKey(QtWidgets.QGraphicsRectItem):
         super(PianoKey, self).mouseReleaseEvent(event)
 
 
-class PianoKeyBoard(QtWidgets.QGraphicsView):
+class PianoKeyBoard(QGraphicsView):
     def __init__(self, num_octaves=7,  parent=None, end_octave=1, first_octave=3):
         super(PianoKeyBoard, self).__init__(parent)
         self.initialize()
         self.m_numOctaves = num_octaves
-        scene = QtWidgets.QGraphicsScene(QtCore.QRectF(0, 0, KEYWIDTH * self.m_numOctaves * 7, KEYHEIGHT), self)
+        scene = QGraphicsScene(QRectF(0, 0, KEYWIDTH * self.m_numOctaves * 7, KEYHEIGHT), self)
         self.setScene(scene)
         numkeys = self.m_numOctaves * 12 + end_octave + first_octave
         shift_octave = 0
@@ -183,26 +199,26 @@ class PianoKeyBoard(QtWidgets.QGraphicsView):
                 if j >= 5: j += 1
             if j % 2 == 0:
                 x = (octave + j / 2) * KEYWIDTH
-                key = PianoKey(rect=QtCore.QRectF(x, 0, KEYWIDTH, KEYHEIGHT), black=False, num=i)
+                key = PianoKey(rect=QRectF(x, 0, KEYWIDTH, KEYHEIGHT), black=False, num=i)
             else:
                 x = (octave + j // 2) * KEYWIDTH + KEYWIDTH * 6 // 10 + 1
-                key = PianoKey(rect=QtCore.QRectF(x, 0, KEYWIDTH * 8 // 10 - 1, KEYHEIGHT * 6//10), black=True)
+                key = PianoKey(rect=QRectF(x, 0, KEYWIDTH * 8 // 10 - 1, KEYHEIGHT * 6//10), black=True)
                 key.setZValue(1)
-            key.setPressedBrush(QtWidgets.QApplication.palette().highlight())
+            key.setPressedBrush(QApplication.palette().highlight())
             key.m_number = i
             self.scene().addItem(key)
 
     def initialize(self):
-        self.setAttribute(QtCore.Qt.WA_InputMethodEnabled, False)
-        self.setCacheMode(QtWidgets.QGraphicsView.CacheBackground)
-        self.setViewportUpdateMode(QtWidgets.QGraphicsView.MinimalViewportUpdate)
-        self.setRenderHints(QtGui.QPainter.Antialiasing |
-            QtGui.QPainter.TextAntialiasing |
-            QtGui.QPainter.SmoothPixmapTransform)
-        self.setOptimizationFlag(QtWidgets.QGraphicsView.DontClipPainter, True)
-        self.setOptimizationFlag(QtWidgets.QGraphicsView.DontSavePainterState, True)
-        self.setOptimizationFlag(QtWidgets.QGraphicsView.DontAdjustForAntialiasing, True)
-        self.setBackgroundBrush(QtWidgets.QApplication.palette().base())
+        self.setAttribute(Qt.WA_InputMethodEnabled, False)
+        self.setCacheMode(QGraphicsView.CacheBackground)
+        self.setViewportUpdateMode(QGraphicsView.MinimalViewportUpdate)
+        self.setRenderHints(QPainter.Antialiasing |
+            QPainter.TextAntialiasing |
+            QPainter.SmoothPixmapTransform)
+        self.setOptimizationFlag(QGraphicsView.DontClipPainter, True)
+        self.setOptimizationFlag(QGraphicsView.DontSavePainterState, True)
+        self.setOptimizationFlag(QGraphicsView.DontAdjustForAntialiasing, True)
+        self.setBackgroundBrush(QApplication.palette().base())
 
     def sizeHint(self):
         return self.mapFromScene(self.sceneRect()).boundingRect().size()
@@ -245,12 +261,12 @@ def play_sound():
     play(sound)
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     app.setStyle('fusion')
     ex = Example()
     lol1 = QLineEdit()
     lol1.setMinimumHeight(100)
-    lay = QtWidgets.QVBoxLayout(ex)
+    lay = QVBoxLayout(ex)
     lay.addWidget(menubar)
     lay.addLayout(buttton_lay)
     lay.addWidget(lol1)
