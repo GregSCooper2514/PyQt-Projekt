82efecd9c028feaddeb6da768054c807d99009cc
GregSCooper2514 <kirill.shestov123@gmail.com>
2021-01-11 08:52:11 +0300

    Final

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import os
 import sys
 import time
 import csv
@@ -24,7 +23,6 @@ note_list = {"0": "C-0", "1": "C#-0", "2": "D-0", "3": "C-1", "4": "C#-1", "5": 
              "72": "B#-5", "73": "C-6", "74": "C#-6", "75": "D-6", "76": "D#-6", "77": "E-6",
              "78": "E#-6", "79": "F-6", "80": "F#-6", "81": "G-6", "82": "G#-6", "83": "A-6",
              "84": "A#-6", "85": "B-6", "86": "B#-6", "87": "C-7"}
-os.chdir("C:\\Users\\Greg\\Downloads")
 default_sound = AudioSegment.from_wav("SoundSamples\\default.wav")
 start_time = None
 end_time = None
